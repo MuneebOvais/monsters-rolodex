2e15c79e1137b3cdcdbc88679108033a5f61da81
MuneebOvais <muneebovaiskhan@yahoo.com>
2020-04-22 18:34:16 +0500

    Updates

---
+++ precache-manifest.8e681aee5bb634e59fa5bdd6e72609c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c139f08a9486d3ef4853e81dd6674f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bdb92adc64b8391b84f2",
+    "url": "/monsters-rolodex/static/css/main.3ac637da.chunk.css"
+  },
+  {
+    "revision": "66b0df324354b491920f",
+    "url": "/monsters-rolodex/static/js/2.296bb81f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.296bb81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb92adc64b8391b84f2",
+    "url": "/monsters-rolodex/static/js/main.e8d9eedb.chunk.js"
+  },
+  {
+    "revision": "800b7874dd07cde0d6ac",
+    "url": "/monsters-rolodex/static/js/runtime-main.3d58e35e.js"
+  }
+]);
